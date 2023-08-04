2d01820eaa709c0b9fee72f35b530ab92910bf23
Daffa Kurnia Fatah <daffa@itsavirus.com>
2023-08-04 10:46:17 +0800

    feat: :sparkles: Add function for getting base url

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,4 +1,10 @@
 const IS_SERVER = typeof window === "undefined";
+
+/**
+ * Get URL String Function
+ * @param path URL path as a string
+ * @returns getURL function
+ */
 export default function getURL(path: string) {
   const baseURL = IS_SERVER
     ? process.env.NEXT_PUBLIC_SITE_URL!
